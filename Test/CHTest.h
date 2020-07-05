8eed8f97c8035442648cfbafc7a6cfafd6dbf681
fengyeyefeng <changshugang@163.com>
2020-07-05 09:14:40 +0800

    update :  增加Test打印类

---
+++ Test/CHTest.h
@@ -0,0 +1,18 @@
+//
+//  CHTest.h
+//  Pods-TestLib_Example
+//
+//  Created by shugang.chang on 2020/7/5.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CHTest : NSObject
+
+- (void)printTest;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test/CHTest.m
@@ -0,0 +1,16 @@
+//
+//  CHTest.m
+//  Pods-TestLib_Example
+//
+//  Created by shugang.chang on 2020/7/5.
+//
+
+#import "CHTest.h"
+
+@implementation CHTest
+
+- (void)printTest {
+    NSLog(@"Hello world");
+}
+
+@end
